05d36a66577655ee83e4aaa9b59dbf96d75b5eaf
jsteemann <jan@arangodb.com>
2017-12-16 13:54:37 +0100

    ballern!

--- ballern.cpp
+++ ballern.cpp
@@ -1,9 +1,45 @@
 // ballern!
 #include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+  
+static std::string readInputFile(char const* filename) {
+  std::ifstream ifs(filename, std::ifstream::in);
+  
+  if (!ifs.is_open()) {
+    throw std::string("cannot read input file '") + filename + "'";
+  }
+  
+  std::string result;
+  char buffer[32768];
+  result.reserve(sizeof(buffer));
+
+  while (ifs.good()) {
+    ifs.read(&buffer[0], sizeof(buffer));
+    result.append(buffer, ifs.gcount());
+  }
+  ifs.close();
+ 
+  return result;
+}
 
 int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    std::cerr << "usage: " << argv[0] << " <filename>" << std::endl;
-    std::exit(1);
+  try {
+    if (argc < 2) {
+      throw std::string("usage: ") + argv[0] + " <filename>";
+    }
+
+    char const* infile = argv[1];
+    if (::strcmp(infile, "-") == 0) {
+      // read from pipe hack
+      infile = "/proc/self/fd/0";
+    }
+
+    std::string code = readInputFile(infile);
+    std::cout << "gotta parse this code soon: " << code << std::endl;
+  } catch (std::string const& ex) {
+    std::cerr << ex << std::endl;
+    return 1;
   }
 }
