ef3163f2ed13faf911a171806379301ccfce0482
jsteemann <jan@arangodb.com>
2017-12-16 13:44:50 +0100

    ballern!

--- ballern.cpp
+++ ballern.cpp
@@ -1,4 +1,9 @@
 // ballern!
+#include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    std::cerr << "usage: " << argv[0] << " <filename>" << std::endl;
+    std::exit(1);
+  }
 }
